d29679c1fa6648ce43255b9c8ef33f9ab3e45dd4
Anusha Ranganathan <anusha@cottagelabs.com>
2022-05-06 11:02:10 +0100

    Secrets needs dir and strip spaces and newlines for safety

--- Importer/jctdata/lib/secrets.py
+++ Importer/jctdata/lib/secrets.py
@@ -6,7 +6,7 @@ def get_secret(file_path):
         raise Exception("Secrets file {x} does not exist".format(x=file_path))
 
     with open(file_path) as f:
-        contents = f.read()
+        contents = f.read().strip()
 
     if not contents:
         raise Exception("Secrets file {x} was empty".format(x=file_path))

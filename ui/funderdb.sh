53d09ff3b007986de161af897c660bfed2ed70bb
Anusha Ranganathan <anusha@cottagelabs.com>
2022-05-07 07:00:41 +0100

    Compiled funders and plugin js file

--- ui/funderdb.sh
+++ ui/funderdb.sh
@@ -1,14 +1,9 @@
-# This is a temporary fix to build in the funder database data into the UI before
-# it is available formally via the API/standard build
+# This builds in the list of funders, to save querying the API
 
-FUNDERDB=../jct-funderdb
-FDB2HERE=../jct
+FUNDERDB=../funderdb
+FDB2HERE=../ui
 
-#(cd $FUNDERDB || exit; python config.py)
-(cd $FUNDERDB || exit; python funders.py)
-#(cd $FUNDERDB || exit; python lang.py)
+(cd $FUNDERDB || exit; python3 funders.py)
 (cd $FUNDERDB || exit; git log --pretty=format:'%h' -n 1 > $FDB2HERE/funderdb.git)
 
-#cp $FUNDERDB/config/default_config.js static/js
-#cp $FUNDERDB/language/default_lang.js static/js
 cp $FUNDERDB/autocomplete/funders.js static/js

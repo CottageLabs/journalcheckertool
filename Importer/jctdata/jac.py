68d6474c549f588023332a99ad03a078a6a642d5
Richard Jones <richard@cottagelabs.com>
2022-07-05 20:29:06 +0100

    fix bug with issn clustering algorithm

--- Importer/jctdata/jac.py
+++ Importer/jctdata/jac.py
@@ -133,9 +133,40 @@ def issn_clusters(coincident_issn_files, clusters_file):
     issn_clusters.sort()
     issn_clusters = list(k for k,_ in itertools.groupby(issn_clusters))
 
+    d = {}
+    for row in issn_clusters:
+        if row[0] not in d:
+            d[row[0]] = []
+        d[row[0]] += [row[x + 1] for x in range(len(row) - 1)]
+
+    # go through every key, and for each value in the array associated with it, look
+    # for the key of that value.  This will lead to another set of values.  For each
+    # of those transitive values that are not the original key and are not in the
+    # original list of values, add it to a list of additional values to add.  Then remove
+    # the key of the value, so we don't count it again.
+    #
+    # In this way, we "consume" all the values into their unique super-clusters by
+    # expanding the first list of values with values from all transitive lists.
+    # In theory we end up with a unique list of coincident identifiers, with all
+    # smaller subsets of the larger cluster subsumed into one.
+    keys = list(d.keys())
+    for k in keys:
+        if k not in d:
+            continue
+        additions = []
+        for e in d[k]:
+            if e in d:
+                additions += [x for x in d[e] if x != k and x not in d[k]]
+                del d[e]
+        d[k] = list(set(d[k] + additions))
+
+    rows = []
+    for k, v in d.items():
+        rows.append([k] + v)
+
     with open(clusters_file, "w") as f:
         writer = csv.writer(f)
-        writer.writerows(issn_clusters)
+        writer.writerows(rows)
 
 
 def _cat_and_dedupe(files):

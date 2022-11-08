887b8d262b87112d8518a6e8fdbdbb8caf939432
Richard Jones <richard@cottagelabs.com>
2022-11-08 12:42:33 +0000

    revert api endpoint to correct url

--- ui/static/js/api_endpoint.js
+++ ui/static/js/api_endpoint.js
@@ -1,2 +1,2 @@
-window.JCT_API_endpoint = 'https://api.jct2.cottagelabs.com/';
+window.JCT_API_endpoint = 'https://api.journalcheckertool.org';
 window.JCT_UI_BASE_URL = "https://journalcheckertool.org";

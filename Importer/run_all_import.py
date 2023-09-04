c139bd48e118c6b66421a759d551a1ec18c5d214
Richard Jones <richard@cottagelabs.com>
2023-09-04 16:19:08 +0100

    remove unneeded importer steps

--- Importer/run_all_import.py
+++ Importer/run_all_import.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-import requests
 import json
-from jctdata import settings
 from jctdata.lib.send_mail import send_mail
 from jctdata import cli
 
@@ -11,19 +9,7 @@ messages.append("{x}: Starting importer run.".format(x=datetime.utcnow()))
 
 cli.run("load", ("_all",))
 messages.append("{x}: Ran all local importer processes".format(x=datetime.utcnow()))
-
-# Start jct import
-print('Starting JCT remote import')
-request = requests.get(settings.JCT_IMPORT_URL)
-if request.status_code == 200:
-    messages.append("{x}: Started JCT import.".format(x=datetime.utcnow()))
-    status = 'started'
-else:
-    messages.append("{x}: ERROR starting JCT import.".format(x=datetime.utcnow()))
-    status = 'ERROR starting'
-
 messages.append("{x}: Finished importer run.".format(x=datetime.utcnow()))
 
-subject = "Importer run : {a}".format(a=status)
+subject = "Importer run"
 send_mail(subject, json.dumps(messages, indent=4), None)
-

c6dee40122a985e0092f5223a35eb4dfffa8fb44
Ramakrishna Sakhamuru <rama@cottagelabs.com>
2024-04-11 18:00:15 +0530

    Updated the code to load the data to es

--- Importer/scripts/generate_test_data.py
+++ Importer/scripts/generate_test_data.py
@@ -6,7 +6,8 @@ Usage:
     python generate_test_data.py [<csv_file1> <csv_file2> ... ]
 
 """
-
+import argparse
+import ast
 import csv
 import json
 import os
@@ -14,6 +15,8 @@ import sys
 from collections import defaultdict
 from datetime import datetime, timedelta
 import random
+from jctdata import settings
+
 
 def calculate_issn_check_digit(issn):
     """Calculate the ISSN check digit for the first seven ISSN digits."""
@@ -31,18 +34,23 @@ def generate_issn():
     check_digit = calculate_issn_check_digit(first_seven_digits)
     return f"{first_seven_digits[:4]}-{first_seven_digits[4:]}{check_digit}"
 
+
 def generate_created_at():
     """Generate a dynamic creation datetime string in ISO 8601 format."""
     now = datetime.utcnow() - timedelta(days=random.randint(0, 365), hours=random.randint(0, 23),
                                         minutes=random.randint(0, 59))
     return now.strftime('%Y-%m-%dT%H:%M:%SZ')
 
+
 def parse_header(header):
     """ Parse the header to get the filename and the JSON key. """
     parts = header.split('.')
     return parts[0], '.'.join(parts[1:]) if len(parts) > 1 else None
 
+
 counter = 0
+funder_counter = 0
+
 
 def generate_random_title_and_publisher():
     """Generate a random title and publisher from predefined lists."""
@@ -51,6 +59,16 @@ def generate_random_title_and_publisher():
     title = "Test title " + str(counter)
     publisher = "Test publisher " +str(counter)
     return title, publisher
+
+
+def generate_funder_id_name():
+    """Generate a random id and name for funder."""
+    global funder_counter
+    funder_counter = funder_counter + 1
+    funder_id = "test_funder_" + str(funder_counter)
+    name = "Test Funder " +str(funder_counter)
+    return funder_id, name
+
 
 def generate_jac_entry(issn):
     """Generate an entry for jac.json with test data."""
@@ -66,6 +84,7 @@ def generate_jac_entry(issn):
         }
     }
 
+
 def set_value(dct, keys, value):
     """ Set a value in a nested dictionary based on a list of keys. """
     for key in keys[:-1]:
@@ -74,13 +93,62 @@ def set_value(dct, keys, value):
         dct = dct[key]
     dct[keys[-1]] = value if value != '' else None
 
-def write_json_files(data, written_files, output_dir):
+
+def get_funder_data(route, data):
+    FUNDER_ROW = {"routes": {"self_archiving": {"calculate": True, "rights_retention": "2022-11-01T00:00:00Z",
+                "license": ["cc-by", "cc-by-sa", "cc0"], "embargo": 0}, "fully_oa": {"calculate": True,
+                "license": ["cc-by", "cc-by-sa", "cc0"]}, "hybrid": {"calculate": False,
+                "license": ["cc-by", "cc-by-sa", "cc0"]}, "ta": {"calculate": True,
+                "license": ["cc-by", "cc-by-sa", "cc0"]}, "tj": {"calculate": True}},
+                "card_order": ["fully_oa", "fully_oa_by_exception", "ta", "ta_aq", "tj", "self_archiving", "sa_rr",
+                               "journal_non_compliant", "funder_non_compliant", "institution_non_compliant",
+                               "rights_retention_non_compliant"],
+                "cards": [{"id": "fully_oa", "compliant": True, "match_routes": {"must": ["fully_oa"]},
+                           "match_qualifications": {"not": ["fully_oa.oa_exception_caveat"]}, "preferred": False},
+                          {"id": "fully_oa_by_exception", "compliant": True, "match_routes": {"must": ["fully_oa"]},
+                           "match_qualifications": {"must": ["fully_oa.oa_exception_caveat"]}, "preferred": False},
+                          {"id": "sa_rr", "compliant": True, "match_routes": {"must": ["self_archiving"],
+                            "not": ["fully_oa"]}, "match_qualifications":
+                              {"must": ["self_archiving.rights_retention_author_advice"]}, "preferred": False,
+                           "modal": "sa_rr"}, {"id": "self_archiving", "compliant": True, "match_routes":
+                        {"must": ["self_archiving"], "not": ["fully_oa"]},
+                        "match_qualifications": {"not": ["self_archiving.rights_retention_author_advice"]},
+                        "preferred": False, "modal": "sa"}, {"id": "ta", "compliant": True, "match_routes":
+                        {"must": ["ta"]}, "match_qualifications": {"not": ["ta.corresponding_authors"]},
+                        "preferred": False}, {"id": "ta_aq", "compliant": True, "match_routes": {"must": ["ta"]},
+                        "match_qualifications": {"must": ["ta.corresponding_authors"]}, "preferred": False},
+                        {"id": "tj", "compliant": True, "match_routes": {"must": ["tj"]}, "preferred": False,
+                        "modal": "tj"}, {"id": "journal_non_compliant", "compliant": False, "match_routes":
+                        {"not": ["self_archiving", "fully_oa", "ta", "tj"]}, "preferred": False},
+                        {"id": "funder_non_compliant", "compliant": False, "match_routes":
+                        {"not": ["self_archiving", "fully_oa", "ta", "tj"]}, "preferred": False},
+                        {"id": "institution_non_compliant", "compliant": False, "match_routes": {"not":
+                        ["self_archiving", "fully_oa", "ta", "tj"]}, "preferred": False},
+                          {"id": "rights_retention_non_compliant", "compliant": False, "match_routes":
+                        {"not": ["self_archiving", "fully_oa", "ta", "tj"]}, "preferred": False}],
+                  "id": "testid", "name": "Test Funder", "abbr": "SNSF", "plan_s": "2022-11-01T00:00:00Z",
+                  "apc": {"tj": False}}
+
+    for key, value in data.items():
+        if value:
+            FUNDER_ROW["routes"][route][key] = value
+            funder_id, name = generate_funder_id_name()
+            FUNDER_ROW["id"] = funder_id
+            FUNDER_ROW["name"] = name
+        else:
+            return None
+
+    return FUNDER_ROW
+
+
+def write_json_files(route, data, written_files, output_dir):
     """ Write the organized data to JSON files, each JSON object in a single line. """
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     # Open jac.json file for writing if journal data is present and prepare it for writing
     journal_filename = "journal"
+    funder_filename = "funder_config"
     jac_filename = "jac"
     # Check if there's data for journal (case-insensitive) to process
     journal_data_present = any(filename.lower() == journal_filename for filename in data.keys())
@@ -88,6 +156,8 @@ def write_json_files(data, written_files, output_dir):
         jac_file_path = os.path.join(output_dir, f"{jac_filename}.json")
         mode = 'a' if jac_filename in written_files else 'w'
         jac_file = open(jac_file_path, mode, encoding='utf-8')
+        if mode == 'w':
+            written_files.add(jac_filename)
 
     for filename, contents in data.items():
         file_path = os.path.join(output_dir, f"{filename}.json")
@@ -97,47 +167,78 @@ def write_json_files(data, written_files, output_dir):
                 if filename.lower() == journal_filename:
                     issn = generate_issn()
                     content.update({"issn": [issn], "createdAt": generate_created_at()})
-                    # Directly write the corresponding jac entry
+                    # write the corresponding jac entry
                     jac_entry = generate_jac_entry(issn)
                     json.dump(jac_entry, jac_file)
                     jac_file.write('\n')
-                json.dump(content, json_file)
-                json_file.write('\n')
+                if filename.lower() == funder_filename:
+                    funder_data = get_funder_data(route, content)
+                    # write data to funder config the funder data available
+                    if funder_data:
+                        json.dump(funder_data, json_file)
+                        json_file.write('\n')
+                else:
+                    json.dump(content, json_file)
+                    json_file.write('\n')
         written_files.add(filename)
 
     if journal_filename in data:
         jac_file.close()
         written_files.add(jac_filename)
 
-def csv_to_json(csv_file_paths, output_dir):
-    written_files = set()
-    for csv_file_path in csv_file_paths:
-        data = defaultdict(list)
 
-        with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            headers = reader.fieldnames
+def csv_to_json(route, csv_file_path, output_dir, written_files):
 
-            for row in reader:
-                json_objects = defaultdict(dict)
-                for header in headers:
-                    if '.' in header:
-                        filename, json_key = parse_header(header)
-                        if json_key:
-                            nested_keys = json_key.split('.')
-                            set_value(json_objects[filename], nested_keys, row[header])
+    data = defaultdict(list)
 
-                # Add the created JSON objects to the data
-                for filename, json_obj in json_objects.items():
-                    data[filename].append(json_obj)
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        headers = reader.fieldnames
 
-        write_json_files(data, written_files, output_dir)
+        for row in reader:
+            json_objects = defaultdict(dict)
+            for header in headers:
+                if '.' in header:
+                    filename, json_key = parse_header(header)
+                    if json_key:
+                        nested_keys = json_key.split('.')
+                        if row[header]:
+
+                            try:
+                                set_value(json_objects[filename], nested_keys, ast.literal_eval(row[header]))
+                            except:
+                                print("route : " + route + " header : " + header + " value : " + row[header])
+                                set_value(json_objects[filename], nested_keys, row[header])
+
+            # Add the created JSON objects to the data
+            for filename, json_obj in json_objects.items():
+                data[filename].append(json_obj)
+
+    write_json_files(route, data, written_files, output_dir)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python script.py <csv_file1> <csv_file2> ...")
         sys.exit(1)
 
-    csv_file_paths = sys.argv[1:]
-    output_dir = os.path.join("..", "test_database")  # Output directory one level up
-    csv_to_json(csv_file_paths, output_dir)
+    parser = argparse.ArgumentParser(description="Generate JSON files from specified CSV inputs.")
+    parser.add_argument("--self_archiving", help="Path to the self_archiving CSV file", type=str)
+    parser.add_argument("--fully_oa", help="Path to the fully_oa CSV file", type=str)
+    parser.add_argument("--hybrid", help="Path to the hybrid CSV file", type=str)
+    parser.add_argument("--ta", help="Path to the ta CSV file", type=str)
+    parser.add_argument("--tj", help="Path to the tj CSV file", type=str)
+
+    args = parser.parse_args()
+
+    written_files = set()
+    output_dir = settings.TEST_DATABASE  # Output directory
+    funderdb_funders_dir = os.path.join("..", "..", "funderdb", "funders")
+
+    # Handle each specified CSV file
+    file_paths = [{"self_archiving": args.self_archiving}, {"fully_oa": args.fully_oa},
+                  {"hybrid": args.hybrid}, {"ta": args.ta}, {"tj": args.tj}]  # Add other files as needed
+    for file_path in file_paths:
+        for key, value in file_path.items():
+            if value:
+                csv_to_json(key, value, output_dir, written_files)

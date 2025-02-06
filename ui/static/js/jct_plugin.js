b77e268f13cb1b4bda08675f11edf70b34863c9a
Richard Jones <richard@cottagelabs.com>
2025-02-06 21:04:36 +0000

    add banner text, styles and behaviour

--- ui/static/js/jct_plugin.js
+++ ui/static/js/jct_plugin.js
@@ -465,7 +465,8 @@ jct.messages = {
         institution: "My Institution",
         institution_placeholder: "By ROR or name",
         no_affiliation: "No affiliation",
-        explain: 'Explain this result'
+        explain: 'Explain this result',
+        banner: '<a href="https://coalitions.typeform.com/JCT-survey-2025" target="_blank" rel="noopener">Make the JCT work better for you - Take our survey</a>'
     },
     fr: {
         journal: "Revue",
@@ -475,8 +476,14 @@ jct.messages = {
         institution: "Mon institution",
         institution_placeholder: "Par le ROR ou le nom",
         no_affiliation: "Aucune affiliation",
-        explain: "Explications du résultat"
+        explain: "Explications du résultat",
+        banner: '<a href="https://coalitions.typeform.com/JCT-survey-2025" target="_blank" rel="noopener">Faites en sorte que le JCT fonctionne mieux pour vous - Répondez à notre sondage</a>'
     }
+};
+jct.banner = {
+    from: new Date("2025-01-17"),
+    to: new Date("2025-03-12"),
+    dismissed: false
 };
 
 // some convenience shortcuts
@@ -487,10 +494,23 @@ jct.d.gebc = document.getElementsByClassName;
 ////////////////////////////////////////////////////////
 // HTML Fragments for Application structure
 
+jct.banner_frag = (pos) => {
+    let banner = "";
+    if (window.hasOwnProperty("JCT_WIDGET")) {
+        return banner;
+    }
+    let now = new Date();
+    if (now > jct.banner.from && now < jct.banner.to) {
+        banner = `<div class="col col--1of1 jct_banner_container" id="jct_banner_container_${pos}"><div id="jct_banner">${jct.getText("banner")}</div></div>`;
+    }
+    return banner;
+};
+
 // ----------------------------------------
 // html for input form
 // ----------------------------------------
 jct.inputs_plugin_html = () => {
+    let banner = jct.banner_frag("top");
     return `
     <h2 class="sr-only">Make a query</h2>
     <div class="col col--1of3 expression">
@@ -538,6 +558,7 @@ jct.inputs_plugin_html = () => {
     </div>
     <div class="col col--1of3 suggest" id="jct_suggestinstitution">
     </div>
+    ${banner}
     <div class="loading" id="jct_loading" style="display:none">
         <div class="loading__dots">
             <div></div>
@@ -561,12 +582,16 @@ jct.results_plugin_html = `
 // ----------------------------------------
 // html for tiles_plugin
 // ----------------------------------------
-jct.tiles_plugin_html = `
-    <section class="row" id="jct_paths_results">
-        <h3 class="sr-only">Results</h3>
-    </section>
-    <section class="row" id="jct_jcs_price_data"></section>
-`;
+jct.tiles_plugin_html = () => {
+    let banner = jct.banner_frag("tiles");
+    return `
+        <section class="row" id="jct_paths_results">
+            <h3 class="sr-only">Results</h3>
+        </section>
+        ${banner}
+        <section class="row" id="jct_jcs_price_data"></section>
+    `;
+};
 
 // ----------------------------------------
 // Function to display the selected list of tiles
@@ -1017,6 +1042,11 @@ jct.success = () => {
     jct.d.gebi('jct_notcompliant').style.display = 'none';
     jct.d.gebi("jct_loading").style.display = "none";
 
+    let bc = jct.d.gebi("jct_banner_container_top");
+    if (bc) {
+        bc.style.display = "none";
+    }
+
     let paths_results = jct.d.gebi("jct_paths_results");
     jct._emptyElement(paths_results)
     jct.display_result(js);
@@ -1339,7 +1369,7 @@ jct.setup = (manageUrl=true) => {
 
     jct.d.gebi("jct_inputs_plugin").innerHTML = jct.inputs_plugin_html();   // is a function call because it needs to call the language pack
     jct.d.gebi("jct_results_plugin").innerHTML = jct.results_plugin_html;
-    jct.d.gebi("jct_tiles_plugin").innerHTML = jct.tiles_plugin_html;
+    jct.d.gebi("jct_tiles_plugin").innerHTML = jct.tiles_plugin_html();
     let f = jct.d.gebi("jct_funder");
     jct.suggesting = true;
 
